fdb37e33d5a8312bc03f7fd39b7663e299f8e6f6
Younghyun Kim <dana_03@naver.com>
2024-02-15 14:14:45 +0900

    App update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
       <Routes>
-        <Route path='/Home' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/User' element={<User />} />
         <Route path='/BoardList' element={<BoardList />} />
