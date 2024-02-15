1de281a87a19338245bf128af74e9bd7ad569dc5
Younghyun Kim <dana_03@naver.com>
2024-02-15 14:27:30 +0900

    User update

--- src/User.tsx
+++ src/User.tsx
@@ -1,60 +1,60 @@
 import './User.css'
-// import userIcon from './userIcon.png'
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+import userIcon from './userIcon.png'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // import { useNavigate } from 'react-router-dom';
 
-// interface UserInfo {
-//     name: string;
-//     school: string;
-//     email: string;
-//     createAt: string;
-//     role: string;
-//     level: number;
-//     totalScore: number;
-//     age: number;
-//     pictureUrl: string;
-// }
+interface UserInfo {
+    name: string;
+    school: string;
+    email: string;
+    createAt: string;
+    role: string;
+    level: number;
+    totalScore: number;
+    age: number;
+    pictureUrl: string;
+}
 
 const User = () => {
-    // const [data, setData] = useState<UserInfo | null>(null);
-    // // const navigate = useNavigate();
+    const [data, setData] = useState<UserInfo | null>(null);
+    // const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             // localstorage에 저장했던 토큰 가져오기
-    //             const token = localStorage.getItem('token');
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // localstorage에 저장했던 토큰 가져오기
+                const token = localStorage.getItem('token');
 
-    //             // 헤더에 토큰 추가
-    //             const config = {
-    //                 headers: {
-    //                     Authorization: `Bearer ${token}`
-    //                 }
-    //             };
+                // 헤더에 토큰 추가
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                };
 
-    //             // 서버에 사용자 정보 달라고 get 요청 보내기
-    //             const response = await axios.get('http://52.79.242.2:8080/users/my/info', config);
+                // 서버에 사용자 정보 달라고 get 요청 보내기
+                const response = await axios.get('http://52.79.242.2:8080/users/my/info', config);
 
-    //             setData(response.data);  // 요청 완료시 reponse변수에 서버에서 받은 사용자 정보가 저장될 것
-    //         } catch (error) { // get 실패시 console 메시지 출력
-    //             console.error('Error fetching data:', error);
-    //             // navigate('/Login')
+                setData(response.data);  // 요청 완료시 reponse변수에 서버에서 받은 사용자 정보가 저장될 것
+            } catch (error) { // get 실패시 console 메시지 출력
+                console.error('Error fetching data:', error);
+                // navigate('/Login')
 
-    //         }
-    //     };
+            }
+        };
 
-    //     fetchData();
-    // }, []);
+        fetchData();
+    }, []);
 
 
-    // if (!data) {
-    //     return <div>Loading...</div>;
-    // }
+    if (!data) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <>
-            {/* <div className='userPage'>
+            <div className='userPage'>
                 <div className='userRank'>
                     <div className='userImg'>
                         <img src={data.pictureUrl} alt='유저 아이콘' className='userIcon' />
@@ -77,7 +77,7 @@ const User = () => {
                     <p className='userData'>이메일: {data.email}</p>
                     <p className='userData'>가입일: {data.createAt}</p>
                 </div>
-            </div> */}
+            </div>
             <div> User page </div>
         </>
     )
