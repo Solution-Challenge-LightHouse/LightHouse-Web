811ee0ba8f09dde2d0da8710e6923b92b099a3a6
Younghyun Kim <dana_03@naver.com>
2024-02-15 14:08:58 +0900

    Home fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './Header.tsx'
-import Home from './Home.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Header />
     <App />
-    <Home />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
