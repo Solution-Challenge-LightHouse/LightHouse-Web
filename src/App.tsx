4a0e98bb45235deb8f7c4fda3fea759df80f47e8
Younghyun Kim <dana_03@naver.com>
2024-02-20 19:30:50 +0900

    grade1 update

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,9 @@ import Home from './Home.tsx'
 import Grade from './Grade.tsx'
 import QuestionWrite from './QuestionWrite.tsx'
 import Answer from './Answer.tsx'
-import GradeDetail from './GradeDetail.tsx'
+// import GradeDetail from './GradeDetail.tsx'
 import QuestionDeatil from './Question.tsx'
+import Grade1 from './Grade1.tsx'
 
 function App() {
 
@@ -30,7 +31,8 @@ function App() {
         <Route path='/posts/find/:id' element={<BoardDetail />} />
         <Route path='/BoardWrite' element={<BoardWrite />} />
         <Route path='/Grade' element={<Grade />} />
-        <Route path='/examples/find/:grade' element={<GradeDetail />} />
+        {/* <Route path='/examples/find/:grade' element={<GradeDetail />} /> */}
+        <Route path='/examples/find/:grade' element={<Grade1 />} />
         <Route path='/QuestionWrite' element={<QuestionWrite />} />
         <Route path='/Answer' element={<Answer />} />
       </Routes>
---
+++ src/Grade1.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import './BoardDetail.css';
+
+interface BoardInfo {
+    id: string;
+    userName: string;
+    userLevel: string;
+    title: string;
+    content: string;
+    creatAt: string;
+}
+
+const Grade1: React.FC = () => {
+    const [data, setData] = useState<BoardInfo | null>(null);
+    const { grade } = useParams<{ grade: string }>();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                };
+                const response = await axios.get(`https://lighthouse1.site/examples/find/${grade}`, config);
+                setData(response.data);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                navigate('/Login');
+            }
+        };
+
+        fetchData();
+    }, [navigate, grade]);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr className="boardTitle">
+                        <th>No.</th>
+                        <th>Title</th>
+                        <th>Content</th>
+                        <th>User</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={data.id} className="boardContent">
+                        <td>{data.id}</td>
+                        <td>{data.title}</td>
+                        <td>{data.content}</td>
+                        <td>Lv.{data.userLevel}&nbsp;{data.userName}</td>
+                        <td>{data.creatAt}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default Grade1;
