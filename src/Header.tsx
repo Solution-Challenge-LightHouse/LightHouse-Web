edd2929fec7405031d603ffa030584980eb76d45
Younghyun Kim <dana_03@naver.com>
2024-02-20 13:37:23 +0900

    loginBtn update

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,28 @@
 import { Link, useNavigate } from "react-router-dom"
 import lighthouse2 from './lighthouse2.png';
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import './Header.css'
 
 const Header: FC = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 저장하는 state 추가
     const navigate = useNavigate();
+
+    // 로그인 상태를 로컬 스토리지에 동기화
+    useEffect(() => {
+        const savedIsLoggedIn = localStorage.getItem('isLoggedIn');
+        setIsLoggedIn(savedIsLoggedIn === 'true');
+    }, []);
+
+    // 로그인/로그아웃 버튼 클릭시 동작할 함수 구현
+    const handleLoginLogout = () => {
+        if (isLoggedIn) {
+            localStorage.removeItem('token');
+            localStorage.setItem('isLoggedIn', 'false');
+            setIsLoggedIn(false);
+        } else {
+            navigate('/Login');
+        }
+    };
 
     return (
         <>
@@ -25,8 +43,9 @@ const Header: FC = () => {
                     <Link to='/User' className="nav">내 페이지</Link>
                 </div>
                 <div className="title_login">
-                    <button onClick={() => navigate('/Login')}>Login</button>
-
+                    <button onClick={handleLoginLogout}>
+                        {isLoggedIn ? 'Logout' : 'Login'} {/* 로그인 상태에 따라 버튼 텍스트 변경 */}
+                    </button>
                 </div>
             </div>
 
